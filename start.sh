a870999e85e7a8688637c53392b0d441007fde61
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-02-05 23:55:43 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $root ]
+then
+  mkfs.ext4 $root
+  mount $root /mnt
+else
+  echo "Root partition not added. Run 'export root=<partition>' and re-run the script afterwards."
+  exit
+fi
+
+if [ $boot ]
+then
+  mkfs.fat -F32 $boot
+  mount $boot /mnt/boot
+else
+  echo "Boot partition not added. Run 'export boot=<partition>' and re-run the script afterwards."
+  exit
+fi
+
+if [ $swap ]
+then
+  mkswap $swap
+  swapon $swap
+fi
+
+if [ $home ]
+then
+  mkfs.ext4 $home
+  mount $home /mnt/home
+fi
