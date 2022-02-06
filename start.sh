f32a875707edbf2b29fdca3634e4e9dc0e443bb3
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-02-06 00:43:24 +0000

    last commit on this file?!?!?

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ if [ $root ]
 then
   mkfs.ext4 $root && mount $root /mnt && mkdir /mnt/boot || exit
 else
-  echo "Root partition not added. Run 'export root=<partition>' and re-run the script afterwards."
+  echo "Root partition not added. Read the README of the github repo."
   exit
 fi
 
@@ -12,7 +12,7 @@ if [ $boot ]
 then
   mkfs.fat -F32 $boot && mount $boot /mnt/boot || umount /mnt && exit
 else
-  echo "Boot partition not added. Run 'export boot=<partition>' and re-run the script afterwards."
+  echo "Boot partition not added. Read the README of the github repo."
   umount /mnt
   exit
 fi
@@ -26,3 +26,16 @@ if [ $home ]
 then
   mkfs.ext4 $home && mount $home /mnt/home || umount /mnt && exit
 fi
+
+if ! [ $kernel ]
+then
+  kernel=linux
+fi
+
+pacstrap /mnt base $kernel linux-firmware || exit
+timedatectl set-ntp true
+genfstab -U /mnt >> /mnt/etc/fstab
+curl -L https://raw.githubusercontent.com/miguelrcborges/archinstallscript/main/chroot-script.sh -O chroot-script.sh
+arch-chroot /mnt sh chroot-script.sh
+
+
