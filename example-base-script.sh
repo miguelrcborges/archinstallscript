d36da139900d1941922d539dca8d0ae69df465d8
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-02-07 02:06:30 +0000

    Create example-base-script.sh

---
+++ example-base-script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if ! [ $rootpw ]
+then
+  echo "Add a root password before running this script!"
+fi
+
+if ! [ %userpw ]
+then
+  echo "Add a password for your user before running this script!"
+fi
+
+export boot=/dev/sda1
+export root=/dev/sda2
+export cpu=intel
+
+export username=miguel
+export hostname=pc
+export timezone=Europe/Lisbon
+export installtype=desktop
+export desktop=gnome
+
+export kernel=linux-zen
+
+curl -L -s https://bit.ly/35SjPVH | sh
