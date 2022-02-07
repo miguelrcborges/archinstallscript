81088a08362ac60626a7b5864dc7d0aec95f6dad
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-02-07 03:27:09 +0000

    Update chroot-script.sh

--- chroot-script.sh
+++ chroot-script.sh
@@ -18,7 +18,8 @@ fi
 
 echo $hostname >> /etc/hostname
 
-pacman --noconfirm -S grub efibootmgr os-prober vim networkmanager
+
+pacman --noconfirm -S grub efibootmgr os-prober vim networkmanager base-devel
 
 if [ $cpu ]
 then
@@ -34,5 +35,51 @@ if ! [ $rootpw ]
 then
   rootpw="root"
 fi
+if ! [ $username ]
+then
+  username="user"
+fi
+if ! [ $userpw ]
+then
+  userpw="user"
+fi
 
 echo "root:$rootpw" | chpasswd
+useradd -mg wheel $username
+echo "$username:$userpw" | chpasswd
+sed -i "/^# %wheel ALL=(ALL) ALL/ c%wheel ALL=(ALL) ALL" /etc/sudoers.d
+
+if ! [ $installtype ] || [ $installtype == "minimal" ]
+then
+  exit
+fi
+
+sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+pacman -S --noconfirm xorg xf86-video-amdgpu xf86-video-intel xf86-video-nouveau lib32-mesa noto-fonts noto-fonts-cjk noto-fons-emojis noto-fonts-extra
+
+if [ $installtype == "autorice" ]
+then
+  curl -L https://raw.githubusercontent.com/miguelrcborges/archinstallscript/main/rice.sh -s | sh
+  exit
+fi
+
+if [ $installtype == "desktop" ]
+then
+  case $desktop in
+    kde)
+      pacman -S --noconfirm plasma
+      systemctl enable sddm
+      ;;
+    
+    xfce)
+      pacman -S --noconfirm xfce4 xfce4-goodies lightdm lightdm-gtk-greeter
+      systemctl enable lightdm
+      ;;
+    
+    *)
+      pacman -S --noconfirm gnome
+      systemctl enable gdm
+      ;;
+  esac
+fi
+     
