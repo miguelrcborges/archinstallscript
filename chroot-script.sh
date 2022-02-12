e630ac40448b584d49f01b22837a23ea9aca186c
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-02-12 01:17:50 +0000

    welp

--- chroot-script.sh
+++ chroot-script.sh
@@ -67,7 +67,7 @@ then
 fi
 
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-pacman -Sy --noconfirm xorg xf86-video-amdgpu xf86-video-intel xf86-video-nouveau lib32-mesa noto-fonts noto-fonts-cjk noto-fons-emojis noto-fonts-extra pulseaudio pulseaudio-alsa
+pacman --noconfirm -Sy xorg xf86-video-amdgpu xf86-video-intel xf86-video-nouveau lib32-mesa noto-fonts noto-fonts-cjk noto-fons-emojis noto-fonts-extra pulseaudio pulseaudio-alsa
 sed -i "/^; default-sample-format/ cdefault-sample-format = float32le" /etc/pulse/daemon.conf
 sed -i "/^; default-sample-rate/ cdefault-sample-rate = 48000" /etc/pulse/daemon.conf
 sed -i "/^; alternate-sample-rate / calternate-sample-rate = 44100" /etc/pulse/daemon.conf
