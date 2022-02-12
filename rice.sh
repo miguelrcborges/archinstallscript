c70f47b72e7c7b03cc6800fd4794a79b248905de
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-02-12 02:28:17 +0000

    patch

--- rice.sh
+++ rice.sh
@@ -11,4 +11,4 @@ then
 fi
 
 pacman --noconfirm -S xorg-xrandr xorg-xinit feh alacritty sxhkd bspwm python-pywal zsh git zsh-syntax-highlighting xorg-xsetroot 
-su $username -c "systemctl --user enable pulseaudio && cd && git clone https://github.com/miguelrcborges/archinstallscript.git && mv archinstallscript/autorice/* /home/$username/ && rm -rf archinstallscript && chmod +x /home/$username/.config/bspwm/bspwmrc"
+su $username -c "systemctl --user enable pulseaudio && cd && git clone https://github.com/miguelrcborges/archinstallscript.git && mv archinstallscript/autorice/.zshrc /home/$username/ && mv archinstallscript/autorice/.config /home/$username/ && mv archinstallscript/autorice/.fonts /home/$username/ && rm -rf archinstallscript && chmod +x /home/$username/.config/bspwm/bspwmrc"
