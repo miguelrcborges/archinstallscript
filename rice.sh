42352808b8feaa6d672c0a827feba442f300e99f
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-02-12 01:42:06 +0000

    this was changing root's shell for some reason

--- rice.sh
+++ rice.sh
@@ -11,4 +11,4 @@ then
 fi
 
 pacman --noconfirm -S xorg-xrandr xorg-xinit feh alacritty sxhkd bspwm python-pywal zsh git zsh-syntax-highlighting xorg-xsetroot 
-su $username -c "chsh -s /bin/zsh && systemctl --user enable pulseaudio && cd && git clone https://github.com/miguelrcborges/archinstallscript.git && mv archinstallscript/autorice/* /home/$username/ && rm -rf archinstallscript && chmod +x /home/$username/.config/bspwm/bspwmrc"
+su $username -c "systemctl --user enable pulseaudio && cd && git clone https://github.com/miguelrcborges/archinstallscript.git && mv archinstallscript/autorice/* /home/$username/ && rm -rf archinstallscript && chmod +x /home/$username/.config/bspwm/bspwmrc"
