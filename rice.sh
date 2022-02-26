55717965cbd69d95405de4e4272f4121701972ed
Miguel Borges <mrcb08022002@gmail.com>
2022-02-26 12:31:04 +0000

    debloat

--- rice.sh
+++ rice.sh
@@ -10,5 +10,5 @@ then
   userpw=user
 fi
 
-pacman --noconfirm -S xorg-xrandr xorg-xinit feh alacritty sxhkd bspwm python-pywal zsh git zsh-syntax-highlighting xorg-xsetroot ueberzg
+pacman --noconfirm -S xorg-xrandr xorg-xinit feh alacritty sxhkd zsh zsh-syntax-highlighting xorg-xsetroot ueberzug
 su $username -c "systemctl --user enable pulseaudio && cd && git clone https://github.com/miguelrcborges/archinstallscript.git && mv archinstallscript/autorice/.zshrc /home/$username/ && mv archinstallscript/autorice/.config /home/$username/ && mv archinstallscript/autorice/.fonts /home/$username/ && rm -rf archinstallscript && chmod +x /home/$username/.config/bspwm/bspwmrc"
