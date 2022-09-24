53c663b621c6dc1920159437d7fc58a8ec530518
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-09-24 18:37:52 +0000

    Add pipewire-jack

--- chroot-script.sh
+++ chroot-script.sh
@@ -72,7 +72,7 @@ fi
 
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Sy
-pacman --noconfirm -S xorg lib32-mesa noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra pipewire lib32-pipewire wireplumber pipewire-alsa pipewire-pulse
+pacman --noconfirm -S xorg lib32-mesa noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra pipewire lib32-pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack
 
 case $gpu in 
   nvidia)
