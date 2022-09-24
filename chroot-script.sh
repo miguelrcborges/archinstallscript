fe0a6ea7dd7063654ebf7f9b994de89d437cd836
Miguel Rodrigues Carrilho Borges <mrcb08022002@gmail.com>
2022-09-24 18:29:04 +0000

    Add gdm to gnome desktop

--- chroot-script.sh
+++ chroot-script.sh
@@ -107,7 +107,7 @@ then
       ;;
     
     *)
-      pacman -S --noconfirm mutter gnome-shell gnome-session nautilus gnome-control-center gnome-tweaks xdg-desktop-portal-gnome
+      pacman -S --noconfirm mutter gnome-shell gnome-session nautilus gnome-control-center gnome-tweaks xdg-desktop-portal-gnome gdm
       systemctl enable gdm
       ;;
   esac
